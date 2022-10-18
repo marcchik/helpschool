124ec2b6fc649e266f806e01891631a5746685f1
mark <markstal2002@gmail.com>
2022-10-18 18:07:26 +0300

    feat: add structure project

---
+++ helpschool/index.php
@@ -0,0 +1,19 @@
+<? include_once "./template/header.php" ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Help School</title>
+</head>
+<body>
+Hello Pipl
+</body>
+</html>
+
+<? include_once "./template/footer.php" ?>
+
+
